583c39a9377a37cfeab70259d5b5089d188f7403
mastermind222 <60010884+mastermind222@users.noreply.github.com>
2020-10-22 03:02:35 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FastestFinger
-Web based game to check typing speed
+This is a game where your typing speed is tested. Type each word in a given time slot depending on the difficulty level and check your score.
---
+++ js/javascript.js
@@ -0,0 +1,127 @@
+window.addEventListener('load', init);
+
+const levels = {
+  easy: 7,
+  medium: 5,
+  hard: 3
+};
+
+const currentLevel = levels.medium;
+
+
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+
+
+
+
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+
+
+
+
+const words=[
+'hat',
+  'river',
+  'lucky',
+  'statue',
+  'generate',
+  'stubborn',
+  'cocktail',
+  'runaway',
+  'joke',
+  'developer',
+  'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+    ];
+  
+  
+  
+    function init()
+    {
+    	seconds.innerHTML=currentLevel;
+    	displayWord(words);
+    	wordInput.addEventListener('input', startMatch);
+      setInterval(countdown, 1000);
+    	setInterval(checkStatus, 50);
+    }
+
+
+
+     function startMatch() {
+     if (matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        displayWord(words);
+        wordInput.value = '';
+        score++;
+     }
+
+  
+       if (score === -1) {
+         scoreDisplay.innerHTML = 0;
+        } else {
+          scoreDisplay.innerHTML = score;
+    }
+    }
+
+    function matchWords() {
+      if (wordInput.value === currentWord.innerHTML) {
+       message.innerHTML = 'Correct!!!';
+       return true;
+      } else {
+        message.innerHTML = '';
+         return false;
+      }
+    }
+
+    
+    function displayWord(words)
+    {
+    	const indx=Math.floor(Math.random()*words.length);
+    	currentWord.innerHTML=words[indx];
+    }
+
+     
+    
+     function countdown() {
+  
+      if (time > 0) {
+        time--;
+      } else if (time === 0) {
+   
+      isPlaying = false;
+    }
+ 
+     timeDisplay.innerHTML = time;
+    }
+    
+
+    function checkStatus() {
+        if (!isPlaying && time === 0) {
+           message.innerHTML = 'Game Over!!!';
+           score = -1;
+        }
+    }
+   
